a3a07c34888eb1d0b53d5674d2f421a19b7a66b3
Manuel Carvalho <burrowsrd@gmail.com>
2021-02-10 15:03:35 +1100

    Added Comprehension (2)

--- main.py
+++ main.py
@@ -26,3 +26,28 @@ print(squared_nums)
 
 even_nums = [n for n in numbers if n % 2 == 0]
 print(even_nums)
+
+print("*" * 80)
+
+nums1 = []
+nums2 = []
+
+with open('file1.txt') as f:
+    list1 = f.readlines()
+    for line in list1:
+        num = int(line.strip())
+        nums1.append(num)
+
+print(nums1)
+
+with open('file2.txt') as f:
+    list2 = f.readlines()
+    for line in list2:
+        num = int(line.strip())
+        nums2.append(num)
+
+print(nums2)
+
+dif_nums = [n for n in nums1 if n in nums2]
+
+print(dif_nums)
